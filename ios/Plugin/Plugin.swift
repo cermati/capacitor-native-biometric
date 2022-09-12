d5bf99f91dee8b2e14b2fa9381163f212f2461c7
Ravedya Aufa Amaranggana <ravedya.widiyanto@cermati.com>
2022-09-12 13:34:08 +0700

    feat(ios): create public key generation and signing

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -22,6 +22,9 @@ public class NativeBiometric: CAPPlugin {
         case unhandledError(status: OSStatus)
     }
     
+    let publicKeyTag = "CermatiPublicKey"
+    let privateKeyTag = "CermatiPrivateKey"
+    
     typealias JSObject = [String:Any]
     
     @objc func isAvailable(_ call: CAPPluginCall) {
@@ -31,18 +34,18 @@ public class NativeBiometric: CAPPlugin {
         
         obj["isAvailable"] = false
         obj["biometryType"] = 0
-
+        
         let useFallback = call.getBool("useFallback", false)
         let policy = useFallback ? LAPolicy.deviceOwnerAuthentication : LAPolicy.deviceOwnerAuthenticationWithBiometrics
         
         if context.canEvaluatePolicy(policy, error: &error){
             switch context.biometryType {
-                case .touchID:
-                    obj["biometryType"] = 1
-                case .faceID:
-                    obj["biometryType"] = 2
-                default:
-                    obj["biomertryType"] = 0
+            case .touchID:
+                obj["biometryType"] = 1
+            case .faceID:
+                obj["biometryType"] = 2
+            default:
+                obj["biomertryType"] = 0
             }
             
             obj["isAvailable"] = true
@@ -55,35 +58,34 @@ public class NativeBiometric: CAPPlugin {
             }
             var errorCode = 0
             switch authError.code {
-                case LAError.biometryNotAvailable.rawValue:
-                    errorCode = 1
-                    
-                case LAError.biometryLockout.rawValue:
-                    errorCode = 2 //"Authentication could not continue because the user has been locked out of biometric authentication, due to failing authentication too many times."
-                    
-                case LAError.biometryNotEnrolled.rawValue:
-                    errorCode = 3//message = "Authentication could not start because the user has not enrolled in biometric authentication."
-                    
-                default:
-                    errorCode = 0 //"Did not find error code on LAError object"
+            case LAError.biometryNotAvailable.rawValue:
+                errorCode = 1
+                
+            case LAError.biometryLockout.rawValue:
+                errorCode = 2 //"Authentication could not continue because the user has been locked out of biometric authentication, due to failing authentication too many times."
+                
+            case LAError.biometryNotEnrolled.rawValue:
+                errorCode = 3//message = "Authentication could not start because the user has not enrolled in biometric authentication."
+                
+            default:
+                errorCode = 0 //"Did not find error code on LAError object"
             }
             obj["errorCode"] = errorCode
             call.resolve(obj)
         }
-                        
     }
     
     @objc func verifyIdentity(_ call: CAPPluginCall){
         let context = LAContext()
         var canEvaluateError: NSError?
-
+        
         let useFallback = call.getBool("useFallback", false)
         let policy = useFallback ? LAPolicy.deviceOwnerAuthentication : LAPolicy.deviceOwnerAuthenticationWithBiometrics
         
         if context.canEvaluatePolicy(policy, error: &canEvaluateError){
             
             let reason = call.getString("reason") ?? "For biometric authentication"
-
+            
             context.evaluatePolicy(policy, localizedReason: reason) { (success, evaluateError) in
                 
                 if success {
@@ -97,7 +99,7 @@ public class NativeBiometric: CAPPlugin {
                     }
                     
                     switch error._code {
-                    
+                        
                     case LAError.authenticationFailed.rawValue:
                         errorCode = "10"
                         
@@ -121,19 +123,19 @@ public class NativeBiometric: CAPPlugin {
                         
                     case LAError.userFallback.rawValue:
                         errorCode = "17"
-
+                        
                     case LAError.biometryNotAvailable.rawValue:
                         errorCode = "1"
-                
+                        
                     case LAError.biometryLockout.rawValue:
                         errorCode = "2" //"Authentication could not continue because the user has been locked out of biometric authentication, due to failing authentication too many times."
-                
+                        
                     case LAError.biometryNotEnrolled.rawValue:
                         errorCode = "3" //message = "Authentication could not start because the user has not enrolled in biometric authentication."
                         
                     default:
                         errorCode = "0" // Biometrics unavailable
-                    }                    
+                    }
                     call.reject(error.localizedDescription, errorCode, error )
                 }
                 
@@ -196,6 +198,117 @@ public class NativeBiometric: CAPPlugin {
         }catch {
             call.reject(error.localizedDescription)
         }
+    }
+    
+    @objc func getPublicKey(_ call: CAPPluginCall){
+        var obj = JSObject()
+        
+        do {
+            let publicKeyFromKeychain = try getPublicFromKeychain()
+            obj["publicKey"] = publicKeyFromKeychain
+            call.resolve(obj)
+        }catch {
+            do{
+                let generatedPublicKey = try generatePublicKey()
+                try storePublicKeyToKeychain(generatedPublicKey)
+                
+                obj["publicKey"] = generatedPublicKey
+                call.resolve(obj)
+            }catch{
+                call.reject("Cannot generate public key")
+            }
+        }
+    }
+    
+    @objc func signData(_ call: CAPPluginCall){
+        var obj = JSObject()
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+            kSecAttrKeySizeInBits as String: 2048,
+            kSecAttrApplicationTag as String: privateKeyTag,
+            kSecReturnRef as String: true
+        ]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess else { return }
+        let privateKey = item as! SecKey
+        
+        guard let challengeString = call.getString("challengeString") else {
+            call.reject("No challenge string was provided")
+            return
+        }
+        
+        guard let messageData = challengeString.data(using: String.Encoding.utf8) else {
+            call.reject("Invalid message to sign")
+            return
+        }
+        
+        do{
+            guard let signData = SecKeyCreateSignature(
+            privateKey,
+            SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256,
+            messageData as CFData, nil) else {
+                call.reject("Cannot sign data")
+                return
+            }
+            
+            let signedData = signData as Data
+            let signedString = signedData.base64EncodedString(options: [])
+            obj["signedData"] = signedString
+            call.resolve(obj)
+        }
+    }
+    
+    func storePublicKeyToKeychain(_ key: SecKey) throws {
+        let tag = publicKeyTag.data(using: .utf8)!
+        let addquery: [String: Any] = [kSecClass as String: kSecClassKey,
+                                       kSecAttrApplicationTag as String: tag,
+                                       kSecValueRef as String: key]
+        
+        let status = SecItemAdd(addquery as CFDictionary, nil)
+        guard status == errSecSuccess else { return }
+    }
+    
+    func getPublicFromKeychain() throws -> SecKey {
+        let getquery: [String: Any] = [kSecClass as String: kSecClassKey,
+                                       kSecAttrApplicationTag as String: publicKeyTag,
+                                       kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+                                       kSecReturnRef as String: true]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(getquery as CFDictionary, &item)
+        guard status == errSecSuccess else { throw KeychainError.noPassword}
+        let key = item as! SecKey
+        
+        return key
+    }
+    
+    func generatePublicKey() throws -> SecKey {
+        let attributes: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+            kSecAttrKeySizeInBits as String: 2048,
+            kSecPrivateKeyAttrs as String: [
+                kSecAttrIsPermanent as String: true,
+                kSecAttrApplicationTag as String: privateKeyTag,
+                
+            ]
+        ]
+        
+        var error: Unmanaged<CFError>?
+        guard let privateKey =
+                SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+            // throw error
+            throw error!.takeRetainedValue() as Error
+        }
+        
+        let publicKey = SecKeyCopyPublicKey(privateKey)!
+        
+        return publicKey
     }
     
     
