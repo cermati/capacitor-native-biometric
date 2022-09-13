ba12c26ee947794f67d6b59a5276bc090c3e3eb4
Ravedya Aufa Amaranggana <ravedya.widiyanto@cermati.com>
2022-09-13 16:02:28 +0700

    cr

--- src/web.ts
+++ src/web.ts
@@ -19,6 +19,7 @@ export class NativeBiometricWeb
   constructor() {
     super();
   }
+
   isAvailable(): Promise<AvailableResult> {
     throw new Error("Method not implemented.");
   }
@@ -26,18 +27,23 @@ export class NativeBiometricWeb
   verifyIdentity(_options?: BiometricOptions): Promise<any> {
     throw new Error("Method not implemented.");
   }
+
   getCredentials(_options: GetCredentialOptions): Promise<Credentials> {
     throw new Error("Method not implemented.");
   }
+
   setCredentials(_options: SetCredentialOptions): Promise<any> {
     throw new Error("Method not implemented.");
   }
+
   deleteCredentials(_options: DeleteCredentialOptions): Promise<any> {
     throw new Error("Method not implemented.");
   }
+
   getPublicKey(): Promise<PublicKey> {
     throw new Error("Method not implemented.");
   }
+  
   signData(_options: SignDataOptions): Promise<SignedData> {
     throw new Error("Method not implemented.");
   }
