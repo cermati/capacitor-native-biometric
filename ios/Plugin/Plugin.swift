77323ce5e4b681f77b1bc25c3e80beca3cc686ee
Ravedya Aufa Amaranggana <ravedya.widiyanto@cermati.com>
2022-10-05 13:36:46 +0700

    fix(ios): remove default value delete credential ios

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -403,7 +403,7 @@ public class NativeBiometric: CAPPlugin {
     }
     
     // Delete user Credentials from Keychain
-    func deleteCredentialsFromKeychain(_ server: String = "CermatiPublicKey" )throws{
+    func deleteCredentialsFromKeychain(_ server: String)throws{
         let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
                                     kSecAttrServer as String: server]
         
